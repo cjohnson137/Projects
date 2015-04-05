7ed4178c9ff9f2d66cac2baa6d32b85fd87fa805
cobaltgalaxy <CJ-MACBOOK@Charless-MacBook-Pro.local>
2015-04-04 20:18:20 -0500

    Created Label
    Created the Heroes Label

---
+++ Johnson_Charles_Project1/build/iphone/build/Johnson_Charles_Project1.build/Debug-iphonesimulator/Johnson_Charles_Project1.build/Objects-normal/i386/OperationQueue.d
@@ -0,0 +1,6 @@
+dependencies: \
+  /Users/CJ-MACBOOK/Documents/Full\ Sail\ University/Course\ 9\ -\ Visual\ Frameworks/VFW1830/Johnson_Charles_Project1/build/iphone/Classes/OperationQueue.m \
+  /Users/CJ-MACBOOK/Documents/Full\ Sail\ University/Course\ 9\ -\ Visual\ Frameworks/VFW1830/Johnson_Charles_Project1/build/iphone/Classes/TiBase.h \
+  /Users/CJ-MACBOOK/Documents/Full\ Sail\ University/Course\ 9\ -\ Visual\ Frameworks/VFW1830/Johnson_Charles_Project1/build/iphone/Classes/TiThreading.h \
+  /Users/CJ-MACBOOK/Documents/Full\ Sail\ University/Course\ 9\ -\ Visual\ Frameworks/VFW1830/Johnson_Charles_Project1/build/iphone/Classes/TiPublicAPI.h \
+  /Users/CJ-MACBOOK/Documents/Full\ Sail\ University/Course\ 9\ -\ Visual\ Frameworks/VFW1830/Johnson_Charles_Project1/build/iphone/Classes/OperationQueue.h
