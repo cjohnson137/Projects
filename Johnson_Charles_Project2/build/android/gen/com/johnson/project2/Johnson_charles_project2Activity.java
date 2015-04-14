87f3d5eaba7fc4bb526964408db18bad9ed0835b
cobaltgalaxy <CJ-MACBOOK@Charless-MacBook-Pro.local>
2015-04-14 10:50:03 -0500

    Detail Page Created
    Created Detailed Page and added image along with description and title.

---
+++ Johnson_Charles_Project2/build/android/gen/com/johnson/project2/Johnson_charles_project2Activity.java
@@ -0,0 +1,7 @@
+package com.johnson.project2;
+
+import org.appcelerator.titanium.TiRootActivity;
+
+public final class Johnson_charles_project2Activity extends TiRootActivity
+{
+}
---
+++ Johnson_Charles_Project2/build/android/gen/com/johnson/project2/Johnson_charles_project2AppInfo.java
@@ -0,0 +1,70 @@
+package com.johnson.project2;
+
+import org.appcelerator.titanium.ITiAppInfo;
+import org.appcelerator.titanium.TiApplication;
+import org.appcelerator.titanium.TiProperties;
+import org.appcelerator.kroll.common.Log;
+
+/* GENERATED CODE
+ * Warning - this class was generated from your application's tiapp.xml
+ * Any changes you make here will be overwritten
+ */
+public final class Johnson_charles_project2AppInfo implements ITiAppInfo
+{
+	private static final String LCAT = "AppInfo";
+
+	public Johnson_charles_project2AppInfo(TiApplication app) {
+	}
+
+	public String getDeployType() {
+		return "development";
+	}
+
+	public String getId() {
+		return "com.johnson.project2";
+	}
+
+	public String getName() {
+		return "Johnson_Charles_Project2";
+	}
+
+	public String getVersion() {
+		return "1.0";
+	}
+
+	public String getPublisher() {
+		return "CJ-MACBOOK";
+	}
+
+	public String getUrl() {
+		return "http://";
+	}
+
+	public String getCopyright() {
+		return "2015 by CJ-MACBOOK";
+	}
+
+	public String getDescription() {
+		return "undefined";
+	}
+
+	public String getIcon() {
+		return "appicon.png";
+	}
+
+	public boolean isAnalyticsEnabled() {
+		return true;
+	}
+
+	public String getGUID() {
+		return "7edd0a7d-35de-43fb-881b-12041f2aaf2c";
+	}
+
+	public boolean isFullscreen() {
+		return false;
+	}
+
+	public String getBuildType() {
+		return "";
+	}
+}
